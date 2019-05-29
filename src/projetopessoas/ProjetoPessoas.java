929003c837b760f77423adbe7986785e727cf471
J4R84S <jarbasstm@hotmail.com>
2019-05-29 20:31:13 -0300

    Atualizado em 29052019

---
+++ src/projetopessoas/ProjetoPessoas.java
@@ -0,0 +1,27 @@
+
+package projetopessoas;
+public class ProjetoPessoas {
+    public static void main(String[] args) {
+        // Programa Principal (Classe Principal)
+        
+       Pessoa p1 = new Pessoa();
+       Aluno p2 = new Aluno();
+       Professor p3 = new Professor ();
+       Funcionario p4 = new Funcionario();
+       
+       p1.setNome("Pedro");
+       p2.setNome("Maria");
+       p3.setNome("Cláudio");
+       p4.setNome("Fabiana");
+       
+       p1.setSexo("M");
+       p4.setIdade(18);
+       
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
+        System.out.println(p4.toString());
+        
+    }
+    
+}
